8c856ccb451861a521888dc0b178168d777c55dc
havuja <havujaperkele@outlook.com>
2020-02-01 18:30:17 +0200

    engine sound and script added for drone

---
+++ Assets/Drone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum DroneAction
+{
+    Down,
+    Drop,
+    Left,
+    Land,
+    Right,
+    Start,
+    Stop,
+    Up
+}
+
+
+public class Drone : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
